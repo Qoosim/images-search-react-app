1bfa37a93d396a06e8489537b1b33c3d6168aee6
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-11 13:08:05 +0100

    Add search button to search for images

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from './ImageCard';
+import ImageSearch from './ImageSearch';
 
 const ImagesList = () => {
   const [images, setImages] = useState([]);
@@ -25,7 +26,12 @@ const ImagesList = () => {
 
   return (
     <div className="container mx-auto">
-      <div className="grid-grid-cols-3 gap-4">
+      <ImageSearch />
+      {
+        loading ? 
+        <h1 className="text-6xl text-center mx-auto">Loading..</h1>
+        :
+        <div className="grid grid-cols-3 gap-4">
         {
           images.map((image) => (
             <ImageCard 
@@ -34,7 +40,7 @@ const ImagesList = () => {
             />
           ))
         }
-      </div>
+      </div>}
     </div>
   )
 }
