238e2f09c8e415811d3e7147cd670f1067a0b76b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-11 11:11:10 +0100

    Create app template and create div card for the image contents

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import ImagesList from './components/ImagesList';
+
+function App() {
+  return (
+    <div>
+      <ImagesList />
+    </div>
+  );
+}
+
+export default App;
